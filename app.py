0bf22f503e5db91546f30e3ec5f297433338832f
atharvadeopujari <atharvadeopujari@gmail.com>
2023-09-18 14:14:13 +0530

    null

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def construct_index(directory_path):
 
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
 
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
 
     documents = SimpleDirectoryReader(directory_path).load_data()
 
@@ -33,7 +33,7 @@ def chatbot(input_text):
 iface = gr.Interface(fn=chatbot,
                      inputs=gr.components.Textbox(lines=7, label="Enter your question"),
                      outputs="text",
-                     title="Ayush 2.0")
+                     title="Shushruta 2.0")
 
 index = construct_index("docs")
 iface.launch(share=True)
