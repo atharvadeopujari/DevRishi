4d3e90f59e80d77ded1e8c7bd41c8ccdece799ec
marvillage <122214707+marvillage@users.noreply.github.com>
2023-09-22 02:45:23 +0530

    Update articlelist.js
    Added 3 Actual Book Names and their Authors

--- src/helpers/articlelist.js
+++ src/helpers/articlelist.js
@@ -1,18 +1,18 @@
 export const MenuList = [
     {
-      name: "Article 1",
-      author:"-Author 1",
-      link: "/event-1", 
+      name: "Secrets of Indian Medicine Ayurveda ",
+      author:"Dr. R. S. Agarwal",
+      link: "/https://drive.google.com/file/d/1UWl7qT6d7v_o-J6c4T7Bg6qBKwJq5YHj/view", 
     },
     {
-      name: "Article  2",
-      author:"-Author 1",
-      link: "/event-2", 
+      name: "The Magic of Ayurveda",
+      author:"Sanjay Dixit,Deep Narayan Pandey",
+      link: "/https://drive.google.com/file/d/1C2S0y9XSRaK-6-3uRafe4SugArOsow7X/view", 
     },
     {
-      name: "Article 3",
-      author:"-Author 1",
-      link: "/event-3", 
+      name: "Shushrutha samhita",
+      author:"K.L Bhishagratna",
+      link: "/https://drive.google.com/file/d/14gst5YTpt-zNdaMEizv_0rMcHNA2r4xT/view3", 
     },
     
     
