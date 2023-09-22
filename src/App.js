eb30e1d0a2802d2d111813dee93eeba638d3318e
marvillage <122214707+marvillage@users.noreply.github.com>
2023-09-22 19:40:05 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,25 +3,29 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Home from "./pages/home";
 import Content1 from "./pages/content1";
+import Content2 from "./pages/content2";
+import Content3 from "./pages/content3";
+import Content4 from "./pages/content4";
 import Book from "./pages/ebooksandpdf"; 
 import Navbar from "./components/Navbar"; 
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router> {/* Wrap your entire app with the Router */}
+      <div className="App">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/content1" element={<Content1 />} />
+          <Route path="/content2" element={<Content2 />} />
+          <Route path="/content3" element={<Content3 />} />
+          <Route path="/content4" element={<Content4 />} />
           <Route path="/book" element={<Book />} /> 
         </Routes>
-        {/* Add a link to navigate to the Content1 component */}
-        <Link to="/content1">Go to Content1</Link>
-      </Router>
+      </div>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
